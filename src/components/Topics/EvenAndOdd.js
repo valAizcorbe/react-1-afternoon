7a9fd39f6b8cce36e8e274b1081dcaecddb21399
valAizcorbe <valeria.aizcorbe@hotmail.com>
2019-09-23 14:54:41 -0600

    not finished

---
+++ src/components/Topics/EvenAndOdd.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class EvenAdnOdd extends Component {
+    render(){
+        return (
+            <p>EvenAdnOdd Component</p>
+        )
+    }
+}
